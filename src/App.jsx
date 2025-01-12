6e791d4fc69e099d371b54d238db792a8723d9c8
nimeshdev11 <nimeshthakur009@gmail.com>
2025-01-12 17:04:36 +0530

    api setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+import { fetchDataFromAPi } from "./utils/api";
+
 function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = async () => {
+    try {
+      const res = await fetchDataFromAPi("/movie/popular");
+      const { results } = res;
+      console.log(results[10].id); // Logs the ID at index 10
+      setResults(results); // Update state with fetched results
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   return (
     <>
-      <div>This is Movix Application</div>
+      <div>This is Movix App</div>
     </>
   );
 }
